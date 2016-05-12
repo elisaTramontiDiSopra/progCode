258aa9e098fb029ad4437a6d4c38addbd1a061f7
elisa.tramontiDiSopra <elisa.tramontidisopra@gmail.com>
2016-05-12 14:06:02 +0200

    MYSQL + PHP fixed query TOTEM still need fixing JSON output

--- totem/php/updateCoda.php
+++ totem/php/updateCoda.php
@@ -4,12 +4,11 @@
    $studio = 'studio01';
    $data = '05052016';
    $ambulatorio = $_GET['ambulatorio'];
-   echo 'ambulatorio'.$ambulatorio;  
 
    $sql = "UPDATE ".$studio." SET ultimoNumeroPrenotato=ultimoNumeroPrenotato+1 WHERE ambulatorio='".$ambulatorio." AND data ='".$data."'";    
    $incrementaCoda = mysqli_query($conn, $sql);  
    
-   $selectNumeroDaStampare = "SELECT ultimoNumeroPrenotato FROM ".$studio." WHERE ambulatorio='".$ambulatorio;
+   $selectNumeroDaStampare = "SELECT ultimoNumeroPrenotato FROM ".$studio." WHERE ambulatorio='".$ambulatorio."'";
    $selezionaUltimoNumero = mysqli_query($conn, $selectNumeroDaStampare);
    if(! $selezionaUltimoNumero ) {
       die('CASINI CON MYSQL: ' . mysql_error());
@@ -23,7 +22,7 @@
     
     $codaAmbulatorio = "codaAmbulatorio".$ambulatorio.".json";
     //write json data into data.json file
-	if(file_put_contents($codaAmbulatorio, $arrayJSON)) {
+	if(file_put_contents($codaAmbulatorio, json_encode($arrayJSON, JSON_PRETTY_PRINT))) {
         //echo 'Dati salvati';
 	}
 	else {
